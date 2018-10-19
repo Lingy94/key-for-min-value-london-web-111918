df08af9e68eb64c2f460293414f93d0cd61f920b
Jack Ling <j.lingyx@gmail.com>
2018-10-19 17:15:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,15 +6,13 @@ def key_for_min_value(name_hash)
   array = name_hash.collect {|key,value| key }
   swapped = true
   n = array.length
-  counter = 1
   while swapped do 
     swapped = false 
-    (n-counter).times do |i|
+    (n-1).times do |i|
       if name_hash[array[i]] > name_hash[array[i + 1]]
         array[i], array[i + 1] = array[i + 1], array[i]
         swapped = true
       end
-      counter += 1
     end
     
   end
